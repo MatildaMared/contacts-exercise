14758c0f028ada2d47c268426e49efb053efb2ef
Matilda Mared <matildamared@live.se>
2022-01-20 11:22:14 +0100

    add tests for ContactList component

--- src/components/contactList/ContactList.test.tsx
+++ src/components/contactList/ContactList.test.tsx
@@ -1,11 +1,42 @@
-import { render } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import ContactList from "./ContactList";
 
 describe("ContactList component", () => {
   it("renders properly", () => {
     render(<ContactList />);
   });
+
+  it("renders a specific contact name", () => {
+    render(<ContactList />);
+
+    const contact = screen.getByText(/Cecilia Circus/);
+
+    expect(contact).toBeInTheDocument();
+  });
+
+  it("renders the correct phone numbers for a specific contact", () => {
+    render(<ContactList />);
+    const contactName = "Bo Bengtsson";
+    const contactPhone = "031-33333333";
+
+    const contact = screen.getByTestId("contact-7830");
+    const nameElement = within(contact).getByText(contactPhone);
+    expect(nameElement).toBeInTheDocument();
+
+    const phoneElement = within(contact).getByText(contactName);
+    expect(phoneElement).toBeInTheDocument();
+  });
+
+  it("renders the correct amount of contacts elements", () => {
+    render(<ContactList />);
+
+    const items = screen.getAllByRole("listitem");
+
+    expect(items.length).toBe(3);
+  });
 });
+
+// Test cases:
 // (1) renders a specific contact name
 // (1) renders the correct phone numbers for a specific contact
 // (1) renders the correct amount of contact elements
--- src/components/contactList/ContactList.tsx
+++ src/components/contactList/ContactList.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Contact } from "../../models/contact";
+
+const data: Contact[] = [
+  {
+    id: "1763",
+    name: "Anna Andersson",
+    phoneNumbers: ["031-12345678", "0733-1234123"],
+  },
+  {
+    id: "7830",
+    name: "Bo Bengtsson",
+    phoneNumbers: ["031-33333333"],
+  },
+  {
+    id: "8042",
+    name: "Cecilia Circus",
+    phoneNumbers: ["031-55555555", "0733-5555555"],
+  },
+];
 
 function ContactList() {
+  const [contacts, setContacts] = useState<Contact[]>(data);
+
   return (
-    <div className="contact-list">
-      <section>
+    <ul className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id} data-testid={"contact-" + contact.id}>
+          <h3>{contact.name}</h3>
+
+          {contact.phoneNumbers.map((phoneNumber) => (
+            <div key={phoneNumber}>{phoneNumber}</div>
+          ))}
+
+          <div className="star">
+            <div>⭐</div>
+            <div>🖊️</div>
+          </div>
+        </li>
+      ))}
+
+      {/* <li data-testid="contact1">
         <h3> Anna Andersson </h3>
         <div>031-12345678</div>
         <div>0733-1234123</div>
@@ -11,18 +47,18 @@ function ContactList() {
           <div>⭐</div>
           <div>🖊️</div>
         </div>
-      </section>
-      <section>
+      </li>
+      <li data-testid="contact2">
         <h3> Bo Bengtsson </h3>
         <div>031-33333333</div>
         <div>0733-4444444</div>
-      </section>
-      <section>
+      </li>
+      <li data-testid="contact3">
         <h3> Cecilia Circus </h3>
         <div>031-55555555</div>
         <div>0733-5555555</div>
-      </section>
-    </div>
+      </li> */}
+    </ul>
   );
 }
 
